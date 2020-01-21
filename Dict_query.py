0e431a82bb45107472782ae35a306bcc42f069e1
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-21 22:38:22 +0530

    Modification:output in List

--- Dict_query.py
+++ Dict_query.py
@@ -2,7 +2,11 @@
 # LIST
 my_dict = {'key1': 21, 'key2': 45, 'key3': 55, 'key4': 14, 'key5': 26}
 value = 30
+list_value =[]
 
 for i,j in my_dict.items():
     if j >= value:
         print(f'Key: ', i, 'Value :', j)
+        list_value.append(j)
+
+print(list_value)

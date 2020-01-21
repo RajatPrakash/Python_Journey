93122e1fbbb2372b683e8ea34d0f9a602b31ed82
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-21 22:36:39 +0530

    Dictonary question

---
+++ Dict_query.py
@@ -0,0 +1,8 @@
+# QUESTION:Write a code to select DICTIONARY elements that are greater then or equal to 30 and place the values in a
+# LIST
+my_dict = {'key1': 21, 'key2': 45, 'key3': 55, 'key4': 14, 'key5': 26}
+value = 30
+
+for i,j in my_dict.items():
+    if j >= value:
+        print(f'Key: ', i, 'Value :', j)

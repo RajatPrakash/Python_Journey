68a76990e8486457e47808efbf0ecd5fb4f4b027
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-22 16:44:24 +0530

    Use of Enumerate in code

---
+++ Enumerate.py
@@ -0,0 +1,6 @@
+# enumerate allows us to loop over something and automatic counter it
+
+my_list = ['apple', 'mango', 'blueberry', 'Kiwi', 'orange']
+
+for i, fruits in enumerate(my_list):
+    print(i, fruits)

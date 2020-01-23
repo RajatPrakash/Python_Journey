59ee44fa64faff47de9a4c28a640ea3d6b03d7d8
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-23 12:11:18 +0530

    WRITE A CODE THAT PRINTS I LOVE YOU FOLLOWED BY YOUR SIGNIFICANT OTHER NAME EVERY 3 SECONDS

---
+++ time_sleep.py
@@ -0,0 +1,6 @@
+# WRITE A CODE THAT PRINTS I LOVE YOU FOLLOWED BY YOUR SIGNIFICANT OTHER NAME EVERY 3 SECONDS
+import time
+while True:
+    value = 'Mom'
+    print('I love you {}'.format(value))
+    time.sleep(3)

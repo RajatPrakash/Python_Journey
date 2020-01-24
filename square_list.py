d0dc491e043177cafb7b2c8c61fde27d50cc9755
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-24 21:58:22 +0530

    SCRIPT WHOSE ITEMS ARE SQUARES OF THE ORIGINAL LIST

---
+++ square_list.py
@@ -0,0 +1,9 @@
+# SCRIPT WHOSE ITEMS ARE SQUARES OF THE ORIGINAL LIST
+
+my_list = list(range(1, 10))
+
+# print(my_list)
+
+square_list = list(map(lambda x: x ** 2, my_list))
+print(my_list)
+print(square_list)

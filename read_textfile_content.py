fe210acb75cacb442eb8434885aca215839fd10a
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-26 22:25:13 +0530

    reading only the first line from text file

--- read_textfile_content.py
+++ read_textfile_content.py
@@ -1,4 +1,6 @@
 # reading data from a file
 
 data = open('Python_codes_list.txt', 'r')
-print(data.read())
+print(data.read())  # .read() will read the whole file
+
+print(data.readline())  # this will read only the first line of the text file

b174f8abfcbaead7a39aa2597ff511d1feae9f86
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-26 22:35:19 +0530

    Creating a file using Python

---
+++ creating_file.py
@@ -0,0 +1,12 @@
+# creating a text file
+
+file = open('new_file.txt', 'a')  # append will open and write , if file does not exits then it will create that file
+
+file.write('''This is the text file that has been created by python code
+
+    CREATE: RAJAT PRAKASH   
+    DATE: 26 JAN 2020
+    
+                            ''')
+
+

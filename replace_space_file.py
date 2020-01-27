f1567d98b4a9871f1c713a13445a7259fe5fee37
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-27 15:17:09 +0530

    Code updated

--- replace_space_file.py
+++ replace_space_file.py
@@ -5,7 +5,8 @@ with open('fruits_input.txt', 'r') as file:
     each_line = file.readlines()
 
 with open('new_file2.txt', 'w') as new_file:
-    new = [item.replace(' ', '') and item.replace('\n', '') and item.replace('!', '') and item.replace('%', '') for item in each_line]
+    new = [item.strip('\n') for item in each_line if '\n' in each_line]
+    # new = [item.strip('!') for item in each_line if '!' in each_line]
     new_file.writelines(new)
     print(new)
 

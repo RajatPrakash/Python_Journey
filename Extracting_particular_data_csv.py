29e25859a23692b3a1fa4b22b168b0d836b3ca5a
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-27 09:53:25 +0530

    update

--- Extracting_particular_data_csv.py
+++ Extracting_particular_data_csv.py
@@ -4,11 +4,19 @@ import csv
 
 new_list = []
 with open('sample_file.csv') as file:
-    data = csv.reader(file,delimiter=',')
+    data = csv.reader(file, delimiter=',')
     records = list(data)
 
-
 for i in records[1:4]:
-    new_list.append.(i[0]+' '+i[1])  #adding first name and last name  from csv file
+    new_list.append(i[0] + ' ' + i[1])  # adding first name and last name  from csv file
 
 print(new_list)
+file.close()
+#
+# with open('sample_file.csv') as f:
+#    file = csv.reader(f, delimiter=',')
+#    output = list(file)
+#
+# print(output)
+#
+# f.close()

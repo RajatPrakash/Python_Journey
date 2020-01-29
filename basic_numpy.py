2f5b6ec34fba187a6be63eb27432ca44a1d91f9e
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-29 14:20:42 +0530

    using numpy basics functions on array

--- basic_numpy.py
+++ basic_numpy.py
@@ -12,3 +12,11 @@ b = np.array([[1.2, 3.2, 1.4], [1.1, 2.2, 3.3]])
 print(b)
 print(' \n')
 
+
+# get the dimension of the array
+print(f'a is {a.ndim}d array')
+print(f'b is {b.ndim}d array')
+
+# get shape of the array, shape is vector
+print(f'a is {a.shape}')
+print(f'b is {b.shape}')

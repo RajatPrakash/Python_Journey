85815e835d79b91a71c8f22a1e086b4642e191f2
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-29 14:16:41 +0530

    initilise the array using numpy

---
+++ basic_numpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# 1d array:
+a = np.array([1, 2, 3, 4])
+print('1-d array ')
+print(a)
+print(' \n')
+
+# 2d array:
+print('2-d array ')
+b = np.array([[1.2, 3.2, 1.4], [1.1, 2.2, 3.3]])
+print(b)
+print(' \n')
+

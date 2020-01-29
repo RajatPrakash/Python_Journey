05466c883c315c3eef90feb7635326e929b4e6b3
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-29 14:34:39 +0530

    accessing array column / rows

--- array_elements.py
+++ array_elements.py
@@ -3,6 +3,8 @@ import numpy as np
 
 a = np.array([[1, 2, 3, 4, 5, 6, 7], [9, 8, 7, 6, 5, 4, 3]])
 print(a.shape)  # array shape
+print(a)
+print('\n')
 
 # specific array element [r,c]
 # item 6
@@ -11,3 +13,9 @@ print(a[0][5])
 # item 8
 print(a[1][1])  # or
 print(a[1][-6])
+
+# get a specific row/specific column
+print(a[1])
+print(a[0])
+print(a[0, :])
+print(a[:, 1])  # prints second column

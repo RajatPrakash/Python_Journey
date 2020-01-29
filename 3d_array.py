798a75f675ce0dc696dca967a03dc4caedb37af7
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-29 14:50:05 +0530

    creating a 3d array and accessing its elements

---
+++ 3d_array.py
@@ -0,0 +1,11 @@
+# creating a 3-d array
+
+import numpy as np
+
+b = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+print(b)
+print('\n')
+# accessing element of a 3-d array
+# element 4
+
+print(b[0, 1, 1])

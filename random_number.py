b871b9e8a684e29976a8d43b98151e2d45c4f099
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-30 14:06:32 +0530

    using random number

---
+++ random_number.py
@@ -0,0 +1,13 @@
+# random number 
+
+
+import numpy as np
+
+# random decimal number array
+a = np.random.rand(2,4)
+print(a)
+print('\n')
+
+# random integer number array
+b = np.random.randint(9,size=(2,3))
+print(b)

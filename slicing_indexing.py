ee01435bfcb48a29c0c11bed528ae629933af4b3
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-31 15:33:29 +0530

    doing slicing and indexing on multi-dimensional array

---
+++ slicing_indexing.py
@@ -0,0 +1,16 @@
+# for slicing and indexing
+import numpy as np
+
+multi_dimensional_array = np.random.randint(10, size=(2, 3))
+print(multi_dimensional_array)
+print('\n')
+
+# element at index 0
+print('Element at index 0 is ')
+print(multi_dimensional_array[0][0])
+print('\n')
+
+# element at index 2 row 3 column
+print("Element at 2 row and 3 column is ")
+print(multi_dimensional_array[1][2])
+print('\n')

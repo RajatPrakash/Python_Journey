bf0835ba984071404fcf60f8810ee5926177456f
Rajat Prakash <rajatprakash904@gmail.com>
2020-02-01 11:28:54 +0530

    accessing first and last rows of first data frame that has been created using Pandas

--- pd_dataframe.py
+++ pd_dataframe.py
@@ -5,3 +5,13 @@ import pandas as pd
 first_df = pd.DataFrame({'employee':[111, 222, 333], 'Name': ['Rajat', 'Aryan', 'Aniket'], 'age': [24, 25, 26]})
 
 print(first_df)
+print('\n')
+
+# accessing the first two rows of data frame
+print('First two rows')
+print(first_df.head(2))
+print('\n')
+
+# accessing the last row only
+print('Last one row')
+print(first_df.tail(1))

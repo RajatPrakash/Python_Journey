43de0ad65d4152ccc563abf540fc69da60fb5031
Rajat Prakash <rajatprakash904@gmail.com>
2020-02-01 11:22:50 +0530

    Creating first data frame using Pandas

---
+++ pd_dataframe.py
@@ -0,0 +1,7 @@
+#creating first data frame
+
+import pandas as pd
+
+first_df = pd.DataFrame({'employee':[111, 222, 333], 'Name': ['Rajat', 'Aryan', 'Aniket'], 'age': [24, 25, 26]})
+
+print(first_df)

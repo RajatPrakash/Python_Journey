15c739067ba7c9b68ad48d0a2f529defe53aed5f
Rajat Prakash <rajatprakash904@gmail.com>
2020-02-01 11:32:46 +0530

    acessing series element using index

--- panda_basics.py
+++ panda_basics.py
@@ -5,3 +5,8 @@ label = ['fruit#1', 'fruit#2', 'fruit#3']
 
 x = pd.Series(data=my_list, index=label)
 print(x)
+print('\n')
+# accessing element of series using its index
+
+print('accessing element using its index')
+print(x['fruit#2'])

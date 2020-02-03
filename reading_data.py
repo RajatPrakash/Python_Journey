45e01204ed93a306131b50a6ea6a89a5454990a9
Rajat Prakash <rajatprakash904@gmail.com>
2020-02-03 15:44:53 +0530

    Ierating through each row using for loop

--- reading_data.py
+++ reading_data.py
@@ -10,11 +10,13 @@ print(data.columns)
 print(data[['Name', 'Attack']][0:5])
 print('\n')
 
-
 # read each rows
-print(data.iloc[1]) # iloc --> integer location based on index
+print(data.iloc[1])  # iloc --> integer location based on index
 print('\n')
 
 # read from specific location (row,column)
 print(data.iloc[2, 1])
 
+# iterate through each row
+for index, row in data.iterrows():
+    print(index, row['Name'])

bb5455dbd935da689275c88eb5ed41faf9bbeaa6
Rajat Prakash <rajatprakash904@gmail.com>
2020-02-03 15:52:22 +0530

    Getting data throug a condition, get the data of only legendary pokemon

--- reading_data.py
+++ reading_data.py
@@ -20,3 +20,12 @@ print(data.iloc[2, 1])
 # iterate through each row
 for index, row in data.iterrows():
     print(index, row['Name'])
+print('\n')
+
+# retrieving  data based on condition
+# printing data of only those pokemon whose power is greater than 50
+print('printing data of only those pokemon whose power is greater than 50')
+print(data.loc[data['HP'] > 50])
+print('\n')
+# get the data of only legendary pokemon
+print(data.loc[data['Legendary'] == True])

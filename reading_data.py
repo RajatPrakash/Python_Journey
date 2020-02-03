5e407aafb5956161602a4048be5e4c8d5b3ece71
Rajat Prakash <rajatprakash904@gmail.com>
2020-02-03 15:41:11 +0530

    reading data from csv file

---
+++ reading_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+data = pd.read_csv('pokemon_data.csv')
+
+# Read headers
+print(data.columns)
+
+# read Each column
+
+print(data[['Name', 'Attack']][0:5])
+print('\n')
+
+
+# read each rows
+print(data.iloc[1]) # iloc --> integer location based on index
+print('\n')
+
+# read from specific location (row,column)
+print(data.iloc[2, 1])
+

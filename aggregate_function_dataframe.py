c91db59b83ab5084a6e468f23fd53b8742fcabb0
Rajat Prakash <rajatprakash904@gmail.com>
2020-02-04 11:06:56 +0530

    counting the list of pokemon from data frame based on Type 1 -- Group by

--- aggregate_function_dataframe.py
+++ aggregate_function_dataframe.py
@@ -6,3 +6,6 @@ data = pd.read_csv('modified_pokemon_data.csv')
 print(data.columns)
 
 print(data.groupby(['Type 1']).mean().sort_values(['Legendary'], ascending=False))  # sorting max legendary value on top
+print('\n')
+# counting the list of pokemon from data frame based on Type 1 -- Group by
+print(data.groupby(['Type 1']).count())

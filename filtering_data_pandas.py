a6613cd2bcb670e8b7ade06fcdedeca301669994
Rajat Prakash <rajatprakash904@gmail.com>
2020-02-04 10:19:41 +0530

    resetting the index for the filtered data

--- filtering_data_pandas.py
+++ filtering_data_pandas.py
@@ -1,11 +1,9 @@
-# filtering data using panda
-
 import pandas as pd
 
-# data = pd.DataFrame({'Employee_id': [111, 222, 333, 444], 'Employee_name': ['Rajat', 'Prateema', 'Malvika',
-# 'Yash'], 'salary': [29000, 32000, 34000, 27000], 'year_of_expr': [2, 4, 5, 6]})
+# data = pd.DataFrame({'Employee_id': [111, 222, 333, 444], 'Employee_name': ['Rajat', 'Prateema', 'Malvika', 'Yash'], 'salary': [29000, 32000, 34000, 27000], 'year_of_expr': [2, 4, 5, 6]})
 
-data = pd.DataFrame({'Student_name': ['akash', 'mansi', 'roshan', 'chandu'], 'age': [15, 17, 19, 14], 'subject': ['maths', 'english', 'physics', 'computer science']})
+data = pd.DataFrame({'Student_name':['akash', 'mansi', 'roshan', 'chandu'], 'age': [15, 17, 19, 14]
+, 'subject': ['maths', 'english', 'physics', 'computer science']})
 
 print(data)
 print('\n')
@@ -19,6 +17,11 @@ print('\n')
 print('Filter student details of maths and physics subject only')
 print(data.loc[(data['subject'] == 'maths') & (data['subject'] == 'physics')])  # no data exists
 
-print(data.loc[(data['subject'] == 'maths') | (data['subject'] == 'physics')])
+new_data = data.loc[(data['subject'] == 'maths') | (data['subject'] == 'physics')]
+print(new_data)
 
+
+# reset index -- after filter and saving data gets stored in old indexing format
+new_data = new_data.reset_index()
+print(new_data)
 # In normal python we use AND OR NOT but in pandas we use the actual symbols &, |, !

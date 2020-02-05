b71ad69e75a0fc3d9371b6f0bc42e682b7af630e
Rajat Prakash <rajatprakash904@gmail.com>
2020-02-05 16:53:28 +0530

    Creating my first matplot lib graph

---
+++ first_matplot_lib.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4]
+y = [5, 6, 7, 8]
+plt.xlabel('Testing for x Axis')
+plt.ylabel('Testing for y Axis')
+plt.title('My first plotting exercise')
+# print(plt.plot(x, y))
+plt.show()

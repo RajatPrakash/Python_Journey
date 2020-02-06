c60ae56238ea875fd29d36ab003252e0e51ecf53
Rajat Prakash <rajatprakash904@gmail.com>
2020-02-06 17:34:52 +0530

    update on legend - use of marker, linewidth

--- legend_matplotlib.py
+++ legend_matplotlib.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 # showing the value of line inside the plot
-x = np.arange(1,10,2)
+x = np.arange(1,100,10)
 y = np.sin(x)
-plt.plot(x,y,label = 'Y = sin(x)')
+plt.plot(x, y, label='Y = sin(x)', linewidth=1.5, marker='*', markersize=5)
 plt.legend()
 plt.xlabel('X Axis')
 plt.ylabel('Y Axis')

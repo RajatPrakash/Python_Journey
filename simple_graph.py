b7a7ec6f4ebf9200105d6ee2e99e46514207ea94
Rajat Prakash <rajatprakash904@gmail.com>
2020-02-06 17:40:45 +0530

    simple graph usng the matplotlib file

---
+++ simple_graph.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+x = [1,2,3]
+y = [2,4,6]
+plt.plot(x,y)
+plt.title('FIrst graph')
+plt.xlabel('X Axis')
+plt.ylabel('y Axis')
+plt.show()

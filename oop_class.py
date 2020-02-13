876ad539d88876af7c2fe18fdfd96326c41aaef0
Rajat Prakash <rajatprakash904@gmail.com>
2020-02-13 13:25:45 +0530

    OOPs Concept : Class

--- oop_class.py
+++ oop_class.py
@@ -2,13 +2,15 @@
 
 
 class Player:
-    def __init__(self, name):
-        self.name = name
-
+    def __init__(self, name, age):  # __init__ is dunder method
+        self.name = name  # attriute or property
+        self.age = age
     def property_run(self):
         print('running')
 
 
-obj = Player('rajat')
-print(obj.name)
-obj.property_run()
+obj = Player('rajat', 23)
+obj2 = Player('Cindy', 25)
+print('Name:', obj.name, 'Age: ', obj.age)
+print('Name:', obj2.name, 'Age: ', obj2.age)
+# obj.property_run()

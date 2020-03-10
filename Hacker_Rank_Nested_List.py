5098394a60b7f6b8f34b3fa6dcb56f1eb2fd531c
Rajat Prakash <rajatprakash904@gmail.com>
2020-03-10 21:22:40 +0530

    Working on Nested list , stuck in comparing only integer values within a list, will complete it ASAP

--- Hacker_Rank_Nested_List.py
+++ Hacker_Rank_Nested_List.py
@@ -2,27 +2,32 @@
 # the name(s) of any student(s) having the second lowest grade.
 # Note: If there are multiple students with the same grade, order their names
 # alphabetically and print each name on a new line.
-#
-# number = int(input('Enter the number of students whose records you want to store:'))
-#
-# items = 2  # name and marks of student
-#
-# my_list = []
-#
-# for i in range(0,number):
-#     while items < 2:
-#         name = input('Name:')
-#         marks = float(input('Marks:'))  # physics marks only
-#         my_list.append(name,marks)
-#         #my_list.append(marks)
-#         j+=1
-#
-# print(my_list)
-new_list=[]
-for i in range(5):
-    new_list.append([])
-    for j in range(5):
-        new_list[i].append(j)
+import numbers
+number = int(input('Enter the number of students whose records you want to store:'))
+
+my_list = []
+
+# Creating nested list which is taking input from the user
+for i in range(number):
+    my_list.append([])
+    for j in range(1):
+        name = input('Name:')
+        marks = int(input('Marks:'))  # physics marks only
+        my_list[i].append(name)
+        my_list[i].append(marks)
+
+print(my_list)
+# c = [x for x in my_list if isinstance(x, (int, float))]
+print(filter(lambda x: isinstance(x, (int, float)),my_list))
+# print(c)
 
 
-print(new_list)
+# nested list
+# new_list=[]
+# for i in range(5):
+#     new_list.append([])
+#     for j in range(5):
+#         new_list[i].append(j)
+#
+#
+# print(new_list)

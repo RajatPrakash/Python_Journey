d01626dd2b0c3f9e6d72c44d88d61884a9fe1545
Rajat Prakash <rajatprakash904@gmail.com>
2020-03-17 15:48:52 +0530

    Creating public,protected and private methods and variables and playing with them (Accessing)

---
+++ public private and protected.py
@@ -0,0 +1,32 @@
+# making varibale states as public / private/ protected
+
+class Test:
+    def __init__(self):
+        self.pub = 'I am Public variable'
+        self._pro = 'I am Protected Variable'
+        self.__pri = 'I am the Private One'
+
+    def puby(self):
+        print(" I am the public method , easily accessible")
+
+    def _proy(self):
+        print(" I am the Protected method , not so easily accessible")
+
+    def __priy(self):
+        print(" I am the PRIVATE method , only single spell can make me accessible")
+
+
+entity = Test()
+
+# lets access the variables
+print(entity.pub)
+print(entity._pro)
+# accessing the private variable
+print(entity._Test__pri)
+
+# lets access the methods
+entity.puby()
+entity._proy()
+
+# accessing the private method
+entity._Test__priy()

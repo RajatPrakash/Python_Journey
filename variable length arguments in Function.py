29e5d395f08d55557c5985932e8fa0a664695ded
Rajat Prakash <rajatprakash904@gmail.com>
2020-03-17 14:41:22 +0530

    using **Kwargs in function

--- variable length arguments in Function.py
+++ variable length arguments in Function.py
@@ -1,4 +1,4 @@
-# working with function where the arguments requirements can be changed
+# working with function where the arguments requirements can be changed *args
 
 
 def min_number(*args):
@@ -10,3 +10,13 @@ def min_number(*args):
 
 
 min_number(-8,2,3,4,5,-66,6,7)
+
+
+# **kwargs (Keyword Arguments)
+
+def user_data(**data):
+    for key,value in data.items():
+        print(f'{key} = {value}')
+
+
+user_data(Firstname='Rajat', LastName='Prakash', Contact=8920416458)

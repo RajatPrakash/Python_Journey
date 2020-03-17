2c9128d2978a8ffb554148bcfcd9b52b3a5e22ab
Rajat Prakash <rajatprakash904@gmail.com>
2020-03-17 09:39:46 +0530

    Using Enumerate Built in function to giing indexes to list values

---
+++ Enumerate_inbuilt.py
@@ -0,0 +1,9 @@
+# working with the Enumerate function
+
+fruits = ['Appple', 'Mango', 'Grapes', 'Banana']
+
+New = list(enumerate(fruits))
+
+print(New)
+
+# output -- [(0, 'Appple'), (1, 'Mango'), (2, 'Grapes'), (3, 'Banana')]

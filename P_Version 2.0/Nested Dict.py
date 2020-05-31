a9283f3783a1aa7df49f1496f1862e3362e1f06a
Rajat Prakash <rajatprakash904@gmail.com>
2020-05-31 15:12:38 +0530

    Nested Dictonary

--- P_Version 2.0/Nested Dict.py
+++ P_Version 2.0/Nested Dict.py
@@ -12,7 +12,9 @@ key1 = input('Enter the 1key: ')
 key2 = input('Enter the 2key: ')
 
 if key1 in dict:
-  print(section[key1])
+  #print(section[key1])
+  print(section[key1][key2])
+
 else:
   print('Invalid Key, try again')
 

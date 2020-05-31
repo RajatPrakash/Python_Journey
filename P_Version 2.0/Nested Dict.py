6bde850d6a9b73029230eb463e9df3e8a4c3e2a7
Rajat Prakash <rajatprakash904@gmail.com>
2020-05-31 15:09:24 +0530

    Started Working on My loose Ends in python, Nested Dictonary

---
+++ P_Version 2.0/Nested Dict.py
@@ -0,0 +1,18 @@
+section = {'A':{'Strength':30,
+                 'girls':12,
+                 'boys':18
+               },
+           'B':{'Strength':45,
+                 'girls':25,
+                 'boys':20
+                }
+          }
+
+key1 = input('Enter the 1key: ')
+key2 = input('Enter the 2key: ')
+
+if key1 in dict:
+  print(section[key1])
+else:
+  print('Invalid Key, try again')
+
